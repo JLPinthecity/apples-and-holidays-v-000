d4be07feef9b33bd47b3e68367e96f20a05a643c
Joann Pan <joannlpan@gmail.com>
2020-05-06 02:16:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -118,7 +118,7 @@ def all_supplies_in_holidays(holiday_hash)
 
   holiday_hash.each do |season, holidays|
     holidays.map do |holiday, supplies|
-      holiday.to_s.capitalize << ":" "#{supplies.join(", ")}"
+      puts "holiday.to_s.capitalize": "#{supplies.join(", ")}"
   
     end
   end
